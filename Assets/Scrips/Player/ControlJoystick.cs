7fa900c76558613ba16ece65c4b880f36cbc2dec
DaveRL83 <posgrado.davidruiz@aragon.unam.mx>
2023-06-08 14:55:22 -0600

    Adecuadiones a la interfaz
    Mejora en el aspecto de la interfaz, se colocó un joystick aunque falta que funciones, me marca error en el script

---
+++ Assets/Scrips/Player/ControlJoystick.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlJoystick : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+/*
+   float horizontalMove = 0;
+    float verticalMove = 0;
+
+    public float runnSpeedHorizontal = 3;
+    public float runnSpeedVertical = 3;
+    public float runnSpeed = 0;
+
+    private static Rigidbody2D playerBody;
+
+    public Joystick joystick;
+
+    void Start()
+    {
+        playerBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalMove = joystick.verticalMove * runnSpeedVertical;
+        horizontalMove = joystick.horizontalMove * runnSpeedHorizontal;
+        transform.position += new Vector3(horizontalMove, verticalMove,0) * Time.deltaTime * runnSpeed;
+    }*/
+}
